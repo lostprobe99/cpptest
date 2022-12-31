c105167cbe434b3bc11079f31a2a50a6f01bc7e4
lostprobe99 <lostprobe@qq.com>
2022-12-31 16:33:10 +0800

    modified co_fib

--- src/multithreading/co_fib.cpp
+++ src/multithreading/co_fib.cpp
@@ -1,51 +1,64 @@
 #include <iostream>
+#include <exception>
 
-struct fib_frame {
+struct fib_frame
+{
     int state = 0;
     int a, b, c;
+    std::exception_ptr exception;
+
+    int resume()
+    {
+        try
+        {
+            switch (state)
+            {
+            case 0:
+                state = 1;
+                goto s0;
+            case 1:
+                state = 2;
+                goto s1;
+            case 2:
+                state = 3;
+                goto s2;
+            case 3:
+                goto s3;
+            }
+
+        s0:
+            a = b = 1;
+            return a;
+        s1:
+            return b;
+        s2:
+            while (true)
+            {
+                c = a + b;
+                return c;
+            s3:
+                a = b;
+                b = c;
+            }
+        }
+        catch (...)
+        {
+            this->exception = std::current_exception();
+        }
+    }
 };
 
-int co_fib(fib_frame &f)
-{
-    switch(f.state)
-    {
-        case 0:
-            f.state = 1;
-            goto s0;
-        case 1:
-            f.state = 2;
-            goto s1;
-        case 2:
-            f.state = 3;
-            goto s2;
-        case 3:
-            goto s3;
-    }
-
-s0:
-    f.a = f.b = 1;
-    return f.a;
-s1:
-    return f.b;
-s2:
-    while(true)
-    {
-        f.c = f.a + f.b;
-        return f.c;
-    s3:
-        f.a = f.b;
-        f.b = f.c;
-    }
-}
-
-int main(int argc, char ** argv)
+int main(int argc, char **argv)
 {
     using std::cout, std::endl;
 
-    fib_frame f;
-    for(int i = 0; i < 10; i++)
+    fib_frame fib;
+    for (int i = 0; i < 10; i++)
     {
-        int x = co_fib(f);
+        int x = fib.resume();
+        if(fib.exception)
+        {
+        }
         cout << x << endl;
     }
 
